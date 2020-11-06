0affae2f1eee44e97920b620e8352ae378602244
harshabakku <bakkubaku@gmail.com>
2020-11-06 17:20:24 +0530

    temp commit eth client smart py code,

--- contracts/tezos/EthClientOnTezos.py
+++ contracts/tezos/EthClientOnTezos.py
@@ -3,20 +3,54 @@
 import smartpy as sp
 
 class EthClientOnTezos(sp.Contract):
-    def __init__(self, initialValue = 5):
-        self.init(storedValue = initialValue,
-                  number =0,
-                  parent_hash = sp.bytes('0x'),
+    def __init__(self):
+        self.init(storedValue = 0,
+                  latest_block =0,
+                  previous_hash = sp.bytes('0x'),
                   header_hash = sp.bytes('0x'),
+                  canonical_header_hashes = sp.map(tkey= sp.TNat , tvalue=sp.TBytes),
+                  known_hashes = sp.set(t=sp.TBytes)
                   
                   )
+                  
         # alert("I'm here" + self.storage.number)    
         
     @sp.entry_point
     def add_block_header(self, params):
-        self.data.parent_hash = params.block_header.parent_hash
-        self.data.number = params.block_header.number
-        self.data.header_hash = params.block_header.hash
+        
+        parent_hash = params.block_header.parent_hash
+        header_hash = params.block_header.hash
+        
+        #  parent hash is not verified for the first block that is being added to eth client 
+        sp.if self.data.latest_block > 0:
+            sp.verify(parent_hash==self.data.previous_hash, message ="Invalid Block: Invalid parent hash" )
+        
+        self.data.latest_block = params.block_header.number
+        self.data.canonical_header_hashes[params.block_header.number] = header_hash
+        self.data.known_hashes.add(header_hash)
+    
+    
+    
+    #  Returns the block hash from the canonical chain.
+    @sp.entry_point
+    def block_hash(self, params):
+        self
+    
+    
+    @sp.entry_point
+    def block_hash_safe(self, params):
+        
+        parent_hash = params.block_header.parent_hash
+        header_hash = params.block_header.hash
+        
+        #  parent hash is not verified for the first block that is being added to eth client 
+        sp.if self.data.latest_block > 0:
+            sp.verify(parent_hash==self.data.previous_hash, message ="Invalid Block: Invalid parent hash" )
+        
+        self.data.latest_block = params.block_header.number
+        self.data.known_hashes.add(header_hash)
+    
+        
     
 @sp.add_test(name="EthClientOnTezos")
 def test():
